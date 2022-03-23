913b90d7a822831aff39ce0a6f89bcc3a828e6c6
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-03-23 07:34:51 +0000

    Fixt a lobby button that turns the lobby off or on

--- communication_center_jitsi/static/src/js/meeting_settings.js
+++ communication_center_jitsi/static/src/js/meeting_settings.js
@@ -12,75 +12,81 @@ odoo.define("communication_center_jitsi.metting_settings.js", function (require)
         if (parent.length === 1){
             parent.append(div);
         }
+
+        let Lobby_onoff = "Off";
+
         const domain = parent.data("jitsi");
 
-        let roomName= parent.data("room_name") //$("#jitsi_meeting_placeholder").data("link_suffix")
+        let roomName= parent.data("room_name")
         console.log(roomName);
 
-        /*
-        roomName += "config.startWithAudioMuted=true&"
-        console.log(roomName)
-    };
-    if ($("#jitsi_meeting_placeholder").data("webcam_off") == "True") {
-        roomName += "config.startWithVideoMuted=true&"
-        console.log(roomName)
-    };*/
-    
-    /*event = {
-        toggleLobby: 'toggle-lobby',
-        displayName: 'display-name'
-    };
-    const commands ={
-        toggleLobby: 'toggle-lobby',
-        displayName: 'display-name'
-    };*/
-    
-    // console.log(roomName);
     const options = {
         roomName: roomName,
         width: 1500,
             height: 700,
             parentNode: div,
-            configOverwrite: { startWithAudioMuted: parent.data("microphone_off") == "True" },
-            //<toggleLobby: 'toggleLobby'
+            configOverwrite: 
+            { 
+                requireDisplayName: parent.data("lobby_with_name") == "True",
+                prejoinPageEnabled: parent.data("lobby_with_name") == "True",
+                startWithAudioMuted: parent.data("microphone_off") == "True", 
+                startWithVideoMuted: parent.data("webcam_off") == "True",
+            },
+            
         };
-        console.log(options.configOverwrite.startWithAudioMuted)
-
-        /*rpc.query ({
-            route: '/controllers/jitsi_controller.py',
-            params: {'options': options},
-        });*/
 
             let api = new JitsiMeetExternalAPI(domain, options);
-
-
-            // api.executeCommand('displayName', 'New Nickname');
-
-            // api.executeCommand('toggleLobby', true);
             
+            if (parent.data("lobby_with_knocking") == "True"){
             api.addEventListener('participantRoleChanged', function (event) {
                 console.log("event", event);
                 if (event.role === 'moderator') {
                     api.executeCommand('toggleLobby', true)
+                    Lobby_onoff = "On";
+                }
+            })};
+
+            var jitsi_button_lobby = $(".jitsi_button_lobby");
+            jitsi_button_lobby.on("click", function (){
+                console.log()
+                if (Lobby_onoff == "On") {
+                    api.executeCommand('toggleLobby', false)
+                    Lobby_onoff = "Off"
+                    alert("Lobby off")
+                    jitsi_button_lobby.while (1); {
+                        jitsi_button_lobby.innerText("Lobby on!")
+                    };
+                } else if (Lobby_onoff == "Off") {
+                    api.executeCommand('toggleLobby', true)
+                    Lobby_onoff = "On"
+                    alert("Lobby on")
+                    jitsi_button_lobby.while (); {
+                        jitsi_button_lobby.innerText("Lobby off!")
+                    };
                 }
             });
 
-            /*api.addEventListener('videoConferenceStart',() => {
-                api.executeCommand('startShareVideo', "https://www.youtube.com/watch?v=JUAYFoRWU5g")
-            });*/
-            // var parent = $('#jitsi_meeting_placeholder');
-            // if (parent.length === 1){
-            //     parent.append(div);
-            // }
             var jitsi_button = $(".jitsi_button");
             jitsi_button.innerText="Hello?"
             jitsi_button.on("click", function (){
                 api.executeCommand('toggleRaiseHand');
-                //console.log(api.executeCommand('startShareVideo', "https://www.youtube.com/watch?v=JUAYFoRWU5g"));
                 console.log('api', api);
             });
-            console.log("event", event);
 
+            if (parent.data("start_recording") == "True"){
+                console.log("REC");
+                api.executeCommand('startRecording')
+            };
+            console.log(parent.data("start_recording"))
+            console.log("HEEYYYY")
+
+            var jitsi_button = $(".jitsi_button_recoff");
+            jitsi_button.on("click", function (){
+                console.log("REC OFF");
+                api.executeCommand('stopRecording', true)
+            });
+
+        console.log("event", event);
     });
 });
 
