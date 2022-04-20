7597a980b302b4e4434f4413203fbdd934c5739c
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-04-20 12:19:49 +0000

    [ADD] Made a new module for markdown

--- website_markdown/__manifest__.py
+++ website_markdown/__manifest__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Website Mardown",
+    # any module necessary for this one to work correctly
+    'depends': ['base',],
+}
