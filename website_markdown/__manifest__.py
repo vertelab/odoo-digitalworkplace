5be62065fac47444e33e3044f3421e1688fbf2c6
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-04-29 11:44:39 +0000

    [IMP] compile markdown inside markdown tags on webpages

--- website_markdown/__manifest__.py
+++ website_markdown/__manifest__.py
@@ -2,7 +2,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': "Website Mardown",
+    'name': "Website Markdown",
     # any module necessary for this one to work correctly
     'depends': ['base',],
+        'external_dependencies': {
+        'python': ['bs4','markdown', ],
+    },
 }
--- website_markdown/models/ir_ui_view.py
+++ website_markdown/models/ir_ui_view.py
@@ -1,10 +1,8 @@
 from odoo import models, fields, api, _
-import logging
+import bs4
 import markdown
-import odoorpc
 
-conn = odoorpc.ODOO()
-_logger = logging.getLogger(__name__+' Kriss')
+MD_TAG = "markdown"
 
 
 class IrUiView(models.Model):
@@ -13,38 +11,14 @@ class IrUiView(models.Model):
     markdown = fields.Text()
 
     def _render_template(self, template, values=None, engine='ir.qweb'):
-        _logger.error(f"{self=}")
-        _logger.error(f"{template=}")
-        _logger.error(f"{engine=}")
-        _logger.error(f"{values=}")
         res = super()._render_template(template, values, engine)
 
-        md = res.decode("utf-8")
-        # if "##" in md:
-        #     return markdown.markdown(md).encode(encoding='UTF-8')
-        #     _logger.error(f"{new_md=}")
-
+        html = res.decode("utf-8")
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        if soup.find(MD_TAG):
+            for x in soup.find_all(MD_TAG):
+                md = markdown.markdown(x.text)
+                soup2 = bs4.BeautifulSoup(md, 'html.parser')
+                x.replace_with(soup2)
+            return str(soup).encode()
         return res
-
-class IrQweb(models.AbstractModel):
-    _inherit = 'ir.qweb'
-
-    @api.model 
-    def _render(self, id_or_xml_id, values=None, **options):
-        # _logger.error(f"{self=}")
-        # _logger.error(f"{id_or_xml_id=}")
-        # _logger.error(f"{options=}")
-        # _logger.error(f"{values=}")
-        res = super()._render(id_or_xml_id, values, **options)
-        #_logger.error(f"{res=}")
-        return res
-
-
-    #     In [34]: for x in html2: 
-    # ...:     if '<' not in x: 
-    # ...:         print(markdown.markdown(x.strip())) 
-    # ...:     else : 
-    # ...:         print (x)
-    #  
-    #     In [49]: for x in soup2: 
-    # ...:     print (markdown.markdown(x.text.strip()))
