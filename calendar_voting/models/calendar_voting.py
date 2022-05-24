a75133ed0ebe8761d669d4493f5e65ce6776f647
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-05-24 09:47:22 +0000

    [IMP] Added a admin for the voting, only voting admin can choose a day and check the checkbox 'Choose This Day', other partisipants gets an error or if a day has not been choosen in the drop down menu

--- calendar_voting/models/calendar_voting.py
+++ calendar_voting/models/calendar_voting.py
@@ -1,4 +1,5 @@
 from calendar import calendar
+from venv import create
 from odoo import _, fields, models, api
 import logging
 from odoo.exceptions import UserError
@@ -11,6 +12,14 @@ class CalendarEvent(models.Model):
 
     voting_checkbox = fields.Boolean(string="Voting")
     participant_ids = fields.One2many(comodel_name="calendar.voting", inverse_name="event_id")
+    choose_day = fields.Selection(
+        selection=[('monday', 'Monday'), 
+        ('tuesday', 'Tuesday'), 
+        ('wednesday', 'Wednesday'), 
+        ('thursday', 'Thursday'), 
+        ('friday', 'Friday')]
+    )
+    choose_this_day = fields.Boolean()
 
     def create_participants(self, partners):
         for partner in partners:
@@ -39,8 +48,24 @@ class CalendarEvent(models.Model):
                 difference = list(voting_partners - event_partners)
                 to_be_removed = record.participant_ids.filtered(lambda p: p.partner_id.id in difference)
                 to_be_removed.unlink()
+            record.choose_a_day(vals)
         return res
 
+    def choose_a_day(self):
+        for record in self:
+            voting_admin = record.user_id
+            if (record.choose_this_day == True) and (voting_admin == self.env.user):
+                if record.choose_day != False:
+                    _logger.error(f"YYYEEEEESSSSS")
+                    #TODO create a funktion for turning off the voting
+                elif record.choose_day == False:
+                    raise UserError(_("Pleas choose a day for the meeting to end the voting"))
+                else:
+                    break
+            elif  (record.choose_day != False) and (voting_admin != self.env.user):
+                    raise UserError(_("You are not allowed to choose day exept for voting admin!"))
+            else:
+                break
 
 class CalendarVoting(models.Model):
     _name='calendar.voting'
@@ -58,5 +83,19 @@ class CalendarVoting(models.Model):
         for record in self:
             if record.partner_id != self.env.user.partner_id:
                 raise UserError(_("You are not allowed to vote for anyone but yourself!"))
+            # elif record.choose_this_day == True:
+            #     raise UserError(_("The voting is over, you are not allowed to vote anymore!"))
         res = super().write(vals)
         return res
+
+
+    #TODO: *create an admin for the metting(the user that created the metting(
+    # *find the id of how created the meeting and compare it to admins id(
+    # *might be able to use name?(
+    # *ansvarig for the meeting?)))),
+    # *create a selector,
+    # *admin can deside witch day the metting will be at from selector/drop down menu, 
+    # and when the day is desided by admin the metting will be over and 
+    # move to that day in the calendar.
+    # use date & date range widget(uvenacc event?) to move meeting maby
+
