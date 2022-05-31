0373c4722294db9234cd2626d8159c021f55a35e
olyrhc <robin.calvin@vertel.se>
2022-05-31 12:32:00 +0000

    calendar_mobile_integration: The latest WIP-version of the code. Parts of it was accidentally overwritten but I managed to salvage some of the more recent changes.

--- calendar_mobile_integration/controllers/main.py
+++ calendar_mobile_integration/controllers/main.py
@@ -2,16 +2,72 @@
 from odoo import http
 from odoo.http import request
 import json
+import datetime
 import logging
 
 _logger = logging.getLogger(__name__)
 
 class CalendarSync(http.Controller):
-    @http.route('/calendar_sync/', auth='user')
+    @http.route('/calendar_sync/', auth='user', csrf=False)
     def index(self, **kw):
+
+        def dateformat(date):
+            date = date.replace('T',' ')
+            date = date.replace('.',' ')
+            return date.split()[0]+' '+date.split()[1]
+
+###     Handle the recieved Android events
         _logger.error(f'{self=}')
-        _logger.error(f'{kw=}')
-#        mydict = {'test': 'Hello, world'}
+        events = []
+        for x in kw:
+            events = json.loads(x)
+
+        for event in events:
+#            _logger.error(f'{event["title"]=}')
+
+            vals = {
+            'name': event['title'],
+            'description': event['notes'],
+            'location': event['location'],
+            'start': dateformat(event['startDate']) ,
+            'stop': dateformat(event['endDate']),
+            'allday': event['allDay'],
+            }
+
+            if 'recurrence' in event:
+                vals['recurrency'] = True
+                vals['rrule_type'] = event['recurrence']['frequency']
+                if 'count' in event['recurrence']:
+                    vals['interval'] = event['recurrence']['count']
+
+            event_id = request.env.ref(f'__sync__.native_calendar_{event["id"]}')
+            if not event_id:
+                event_id = request.env['calendar.event'].create(vals)
+                request.env['ir.model.data'].create({'module':'__sync__', 'name':f'native_calendar_{event['id']}','model':'calendar.event', 'res_id': event_id.id})
+            else:
+                event_id.write(vals)
+
+            if 'attendees' in event:
+                for attendee in event['attendees']:
+                    name = attendee["name"]
+                    email = attendee["email"],
+                    if not name:
+                        name = email
+                    if not email:
+                        continue
+                    partner_id = request.env['res.partner'].search([('email', '=', email)], limit=1)):
+                    if not partner_id:
+                        partner_id = request.env['res.partner'].create({'name':name, 'email':email}
+                    attendee_vals={'event_id':event_id.id, 'partner_id':partner_id.id}
+                    
+
+            _logger.error(f'{vals}')
+
+
+
+###     Odoo events below
+
+        reminders = []
         mydict = {}
         restdata = {}
         events = request.env['calendar.event'].search([])
@@ -51,9 +107,16 @@ class CalendarSync(http.Controller):
             mydict['interval'] = interval
             if until:
                 mydict['until'] = until.strftime('%m/%d/%Y')
-#            mydict[event.id] = {'alarm_ids': alarm_ids}
+            for alarm in alarm_ids:
+                this_alarm = alarm.browse(alarm.id).duration_minutes
+                reminders.append(this_alarm)
+            mydict['reminders'] = reminders
+            reminders = []
+
             restdata[event.id] = mydict
             mydict = {}
+
+
         return json.dumps(restdata)
 #        return http.request.render("calendar_sync.index", {
 #            'test': "Hello, world",
--- calendar_mobile_integration/myapp/app/main-view-model.js
+++ calendar_mobile_integration/myapp/app/main-view-model.js
@@ -1,6 +1,7 @@
-import { Observable } from '@nativescript/core';
+import { login, Observable } from '@nativescript/core';
 import { Http } from '@nativescript/core';
 import { Utils } from "@nativescript/core";
+import { AppSettings } from '@nativescript/core';
 
 function getMessage(counter) {
   if (counter <= 0) {
@@ -19,6 +20,8 @@ export function createViewModel() {
     viewModel.counter--
     viewModel.set('message', getMessage(viewModel.counter))
 
+//    login('Please login to your Odoo account','Name','Password');
+
     Http.request({
       url: "https://calendarapp.azzar.net/web/session/authenticate",
       method: "POST",
@@ -27,9 +30,8 @@ export function createViewModel() {
           params:{
             db: "calendarapp",
             login: "admin",
-            password: "<Paste your password here>",
+            password: "<insert_password_here>",
           }
-
       }),
     })
     .then(
@@ -40,20 +42,74 @@ export function createViewModel() {
         if (jsonobj.error) {
           if (jsonobj.error.data.name === 'psycopg2.OperationalError') {console.log('Database error!');}
           else if (jsonobj.error.data.name === 'odoo.exceptions.AccessDenied') {console.log('Authentication error!');}
-
-
-
         }
         else if (jsonobj.result) {
-          Http.getJSON('https://calendarapp.azzar.net/calendar_sync')
+/*           Http.getJSON('https://calendarapp.azzar.net/calendar_sync')
           .then((result) => {
             viewModel.set('getStringResult', result.test)
             },
           e => {console.log(e);}
-          )
+          ) */
+            getEvents().then(function(events) {
+                console.log(JSON.stringify(events))
+                Http.request({
+                    url: "https://calendarapp.azzar.net/calendar_sync",
+                    method: "POST",
+                    // headers: { "Content-Type": "application/json" },
+                    content: JSON.stringify(
+                        events,
+                    ),
+                })
+                .then(
+                    (response) => {
+                      // Argument (response) is HttpResponse
+                      console.log('---------------------------------');
+                      console.log(java.util.TimeZone.getDefault().getID())
+                      console.log(java.util.Locale.getDefault())
+                        var data = JSON.parse(response.content)
+                        console.log(data)
+
+                        for (const key of data) {
+                          if (key['event_id'] == 38) {
+                            const date = key['start_time']+' UTC'
+                            console.log(new Date(date))
+
+                            const options = {
+                              title: key['name'],
+                              startDate: new Date(date),
+                              endDate: new Date(key['end_time']+' UTC'),
+                              calendar: {
+                                name: "NativeScript Cal",
+                                color: "#00AA00",
+                                accountName: "My App Name"
+                              }
+                            }
+                            console.log(options)
+
+                            Calendar.createEvent(options).then(
+                                function(createdId) {
+                                  console.log("Created Event with ID: " + createdId);
+                                },
+                                function(error) {
+                                  console.log("Error creating an Event: " + error);
+                                }
+                            );
+
+
+
+
+
+//                            viewModel.set('getStringResult', key.description)
+//                            console.log(key)
+                          }
+                        }
+                        console.log(`Response Status Code: ${response.statusCode}`)
+                        console.log(`Response Content: ${response.content}`)
+                    },
+                    e => {}
+                    )
+                })
         }
-
-
       },
       e => {console.log(e);}
       )
@@ -82,8 +138,8 @@ Calendar.hasPermission().then((g)=> console.log("Permission granted?", g ? "YES"
     title: 'Get brains',
     // Make sure these are valid JavaScript Date objects.
     // In this case we schedule an Event for now + 1 hour, lasting 1 hour.
-    startDate: new Date(new Date().getTime() + (60*60*1000)),
-    endDate: new Date(new Date().getTime() + (2*60*60*1000))
+    startDate: new Date(new Date().getTime() ),
+    endDate: new Date(new Date().getTime() + (1000*60*60*24))
   };
 
   // You can however add lots of properties to enrich the Event:
@@ -95,16 +151,16 @@ Calendar.hasPermission().then((g)=> console.log("Permission granted?", g ? "YES"
 
   // You can also override the default reminder(s) of the Calendar (in minutes):
   options.reminders = {
-    first: 30,
+    first: 60,
     second: 10
   };
 
   // You can make this Event recurring (this one repeats every other day for 10 days):
-  options.recurrence = {
+/*   options.recurrence = {
     frequency: "daily", // daily | weekly | monthly | yearly
     interval: 2, // once every 2 days
     endDate: new Date(new Date().getTime() + (10*24*60*60*1000)) // 10 days
-  };
+  }; */
 
   options.attendees = {name:'test testing', email:'test@test.com', url:'myurl', status:'', role:'', type:'Optional'}
 
@@ -114,7 +170,7 @@ Calendar.hasPermission().then((g)=> console.log("Permission granted?", g ? "YES"
   options.calendar = {
     name: "NativeScript Cal",
     // the color, in this case red
-    color: "#FF0000",
+    color: "#00AA00",
     // Can be used on Android to group the calendars. Examples: Your app name, or an emailaddress
     accountName: "My App Name"
   };
@@ -140,25 +196,65 @@ Calendar.hasPermission().then((g)=> console.log("Permission granted?", g ? "YES"
 ) */
 
 
-var options = {
+async function getEvents() {
+  var options = {
   // when searching, dates are mandatory - the event must be within this interval
-  startDate: new Date(new Date().getTime() - (50*24*60*60*1000)),
-  endDate: new Date(new Date().getTime() + (50*24*60*60*1000))
-};
-
+  startDate: new Date(new Date().getTime() - (1000*60*60*24*7)*1 ),
+  endDate: new Date(new Date().getTime() + (1000*60*60*24*30)*6)
+  };
+  console.log('START: '+options.startDate);
+  console.log('END: '+options.endDate);
 // if you know the Event ID, set it here:
-options.id = '71';
+//options.id = '72';
 
 // you can optionally pass in a few other properties, any event containing these will be returned:
-options.title = 'brains';
-options.location = 'foo';
-options.notes = 'bar'; // iOS only
+//options.title = 'Android';
+//options.location = 'foo';
+//options.notes = 'bar'; // iOS only
 
-Calendar.findEvents(options).then(
+
+return await Calendar.findEvents(options).then(
     function(events) {
-      console.log(JSON.stringify(events));
+//      console.log(JSON.stringify(events));
+        console.log('calendar');
+        var calendars = [];
+        for (const obj of events) {
+            if (obj['calendar']['name'].includes('NativeScript')){
+            console.log(obj);
+            calendars.push(obj);
+            }
+        }
+//        console.log(calendars);
+        return Promise.resolve(calendars)
+    },
+    function(error) {
+      alert({
+        title: "Error finding events:",
+        message: error,
+        okButtonText: "Ok"
+      })
+    }
+  );
+//  console.log(obj);
+}
+
+
+
+function calendarEvents(calendarname) {
+  var options = {
+    startDate: new Date(new Date().getTime() - (50*24*60*60*1000)),
+    endDate: new Date(new Date().getTime() + (50*24*60*60*1000))
+  };
+
+  Calendar.findEvents(options).then(
+    function(events) {
+      for (const obj of events) {
+          if (obj['calendar']['name'].includes(calendarname))
+            console.log(obj);
+      }
     },
     function(error) {
       console.log("Error finding Events: " + error);
     }
-);
+  )
+}
