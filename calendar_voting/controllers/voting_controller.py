98b8aa1dc864a81fea381b856b2fe31821e7b7a7
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-06-03 06:45:03 +0000

    [IMP] Cleand upp some code

--- calendar_voting/controllers/voting_controller.py
+++ calendar_voting/controllers/voting_controller.py
@@ -10,3 +10,4 @@ class VotingController(http.Controller):
     @http.route(['/voting_calendar'], type="http", auth='public', website=True)
     def voting_site(self, **kw):
         return request.render("calendar_voting.calendar_voting_site")
+        
