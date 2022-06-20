edf83491b20693485fce9975410340520bf746b2
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-06-20 12:09:54 +0000

    [IMP] Added somme information to thr '__manifest__.py' file

--- calendar_voting/__manifest__.py
+++ calendar_voting/__manifest__.py
@@ -4,8 +4,17 @@
 
 {
     'name': "Calendar Voting",
+    'summary': """Ability to vote for which day a meeting in Odoo should be hold""",
+    'description':"""
+        Ability to vote for which day a meeting in Odoo should be hold
+    """,
+    'category': 'Productivity/VOIP',
+    'sequence': 280,
+    'version': '14.0.0.0.1',
     # any module necessary for this one to work correctly
     'depends': ['base', 'calendar', 'website'],
+
+    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/calendar_voting_view.xml',
