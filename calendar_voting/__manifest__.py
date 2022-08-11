540fa70e8d3f2dd2d9a5008b24cbdad05f759c65
Kriss-Ellen Svensson <kriss-ellen.svensson@vertel.se>
2022-08-11 11:02:48 +0000

    [IMP] Added information to the manifest file and an image

--- calendar_voting/__manifest__.py
+++ calendar_voting/__manifest__.py
@@ -1,16 +1,36 @@
 
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     'name': "Calendar Voting",
+    'author': 'Vertel AB',
     'summary': """Ability to vote for which day a meeting in Odoo should be hold""",
     'description':"""
         Ability to vote for which day a meeting in Odoo should be hold
     """,
-    'category': 'Productivity/VOIP',
-    'sequence': 280,
+    'category': 'Productivity',
     'version': '14.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     # any module necessary for this one to work correctly
     'depends': ['base', 'calendar', 'website'],
 
