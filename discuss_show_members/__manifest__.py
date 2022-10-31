566117e90b5541b57e2374f79bf882b2be11975e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-31 10:56:47 +0100

    Update __manifest__.py

--- discuss_show_members/__manifest__.py
+++ discuss_show_members/__manifest__.py
@@ -1,18 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    "name": "Show Discuss Members",
-    "summary": "Show Discuss Memebers",
-    "version": "14.0.0.1",
-    'category': 'Dicuss',
-    "website": "https://vetelab.com",
-    'author': 'Vertel AB',
-    "license": "AGPL-3",
-    "description": """
-        Show Discuss Members in Channels
+    'name': 'Workspace: Discuss Show Members',
+    'version': '14.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Show Discuss Memebers.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+    'description': """
+    Show Discuss Members in Channels.
     """,
-    "depends": [
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-digitalworkplace/discuss_show_members',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-digitalworkplace',
+    # Any module necessary for this one to work correctly
+    'depends': [
         "mail",
     ],
     "data": [
@@ -26,3 +56,4 @@
     'auto_install': True,
     'license': 'LGPL-3',
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
