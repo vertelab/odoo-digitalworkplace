826bc5c8f546cdf368300c5d064a42e93019db3d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-31 09:35:02 +0100

    Update __manifest__.py

--- communication_center_jitsi/__manifest__.py
+++ communication_center_jitsi/__manifest__.py
@@ -1,18 +1,47 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': "Communication Center Jitsi",
-    'summary': """Setup communication with Jitsi""",
-    'description': """
-        Setup communication with Jitsi
-    """,
-    'category': 'Productivity/VOIP',
-    'sequence': 280,
+    'name': 'Workspace: Communication Center Jitsi',
     'version': '14.0.0.0.1',
-    # any module necessary for this one to work correctly
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Setup communication with Jitsi.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+    'description': """
+    Setup communication with Jitsi.
+    """,
+    #'sequence': '1',
+    'sequence': '280',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-digitalworkplace/communication_center_jitsi',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-digitalworkplace',
+    # Any module necessary for this one to work correctly
     'depends': ['base', 'communication_center_calendar_online_meeting', 'website',],
-
     # always loaded
     'data': [
         'views/res_config_settings_view.xml',
@@ -24,5 +53,5 @@
         'static/src/xml/calendar_view_templet.xml',
     ],
     'application': True,
-    'license': 'OEEL-1',
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
