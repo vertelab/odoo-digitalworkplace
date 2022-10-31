5e6fea84710ea39f6dc93a26213a5a445a823df3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-31 10:38:51 +0100

    Update __manifest__.py

--- communication_center_massmail_meeting/__manifest__.py
+++ communication_center_massmail_meeting/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Workspace: Mass Mailing Meeting Link',
+    'name': 'Workspace: Communication Center Massmail Meeting',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Setup Jitsi for Mass Mailing.',
