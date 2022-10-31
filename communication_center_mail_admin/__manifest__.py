e0a365e17d909f61f9fe2559ae83b395e132ba94
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-31 09:46:33 +0100

    Update __manifest__.py

--- communication_center_mail_admin/__manifest__.py
+++ communication_center_mail_admin/__manifest__.py
@@ -1,18 +1,47 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    "name": "Odoo Chatter Message Edit And Delete",
-    "summary": "Edit and Delete option for chatter's Message & Log Note",
-    "category": "Discuss",
-    "version": "14.0.1.0.0",
-    "sequence": 1,
-    "author": "Vertel AB Ltd.",
-    "website": "https://vertel.se",
-    "description": """
-Log Note
-====================
-This module will add edit/delete option in chatter's message & log note .
+    'name': 'Workspace: Communication Center Mail Admin',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Edit and Delete option for chatter''s Message & Log Note.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+    'description': """
+    Log Note
+    ====================
+    This module will add edit/delete option in chatter's message & log note .
     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-digitalworkplace/communication_center_mail_admin',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-digitalworkplace',
+    # Any module necessary for this one to work correctly
     "depends": ['mail'],
     "data": [
         'security/security.xml',
@@ -31,3 +60,4 @@ This module will add edit/delete option in chatter's message & log note .
     "auto_install": False,
     "pre_init_hook": "pre_init_check",
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
