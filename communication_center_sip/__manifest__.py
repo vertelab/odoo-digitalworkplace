645cdabdcf4b88ea000581f422b013411720d47e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-31 10:41:20 +0100

    Update __manifest__.py

--- communication_center_sip/__manifest__.py
+++ communication_center_sip/__manifest__.py
@@ -1,16 +1,46 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': "Communication Center SIP",
-    'summary': """Make calls using a VOIP system""",
-    'description': """
-        Allows to make call from next activities or with click-to-dial.
-    """,
-    'category': 'Productivity/VOIP',
-    'sequence': 280,
+    'name': 'Workspace: Communication Center SIP',
     'version': '14.0.0.0.1',
-    # any module necessary for this one to work correctly
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Make calls using a VOIP system.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+    'description': """
+    Allows to make call from next activities or with click-to-dial.
+    """,
+    #'sequence': '1',
+    'sequence': '280',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-digitalworkplace/communication_center_sip',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-digitalworkplace',
+    # Any module necessary for this one to work correctly
     'depends': ['base', 'mail', 'web', 'phone_validation'],
 
     # always loaded
@@ -29,3 +59,4 @@
     'application': True,
     'license': 'OEEL-1',
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
