78bdfac8d03c298c3c15232a1a957e9eb9c02c63
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-01 14:49:33 +0100

    Update __manifest__.py

--- user_settings_dav/__manifest__.py
+++ user_settings_dav/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Workspace: User Settings Dav',
+    'name': 'Workplace: User Settings Dav',
     'version': '14.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
