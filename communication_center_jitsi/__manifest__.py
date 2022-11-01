86e7898783866894a1c06519630da0a9e69f13bf
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-01 14:46:41 +0100

    Update __manifest__.py

--- communication_center_jitsi/__manifest__.py
+++ communication_center_jitsi/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Workspace: Communication Center Jitsi',
+    'name': 'Workplace: Communication Center Jitsi',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Setup communication with Jitsi.',
