45fc55cf3a2b74c915cf0c07fe1d75f489d67395
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-01 14:47:50 +0100

    Update __manifest__.py

--- communication_center_mail_admin/__manifest__.py
+++ communication_center_mail_admin/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Workspace: Communication Center Mail Admin',
+    'name': 'Workplace: Communication Center Mail Admin',
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Edit and Delete option for chatter''s Message & Log Note.',
