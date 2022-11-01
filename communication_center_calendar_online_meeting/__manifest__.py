26c593399f0250b360974f2aaa01b548515ac27e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-01 14:46:15 +0100

    Update __manifest__.py

--- communication_center_calendar_online_meeting/__manifest__.py
+++ communication_center_calendar_online_meeting/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Workspace: Communication Center Calendar Online Meeting',
+    'name': 'Workplace: Communication Center Calendar Online Meeting',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Setup a link for your online meeting.',
