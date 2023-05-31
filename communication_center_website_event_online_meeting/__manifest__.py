2780d9730aa57487ea68cb5e5b0b7269642ffc95
Kriss-Ellen Svensson <kriss@zen.vertel.se>
2023-05-31 10:00:56 +0200

    testing intiual melding

---
+++ communication_center_website_event_online_meeting/__manifest__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Workplace: Communication Center Website Event Online Meeting',
+    'version': '16.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Setup a link for your online event',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity',
+    'description': """
+    Setup a link for your online link for events.
+    """,
+    #'sequence': '1',
+    'sequence': '280',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-digitalworkplace/communication_center_website_event_online_meeting',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-digitalworkplace',
+    # Any module necessary for this one to work correctly
+    'depends': ['base', 'event', 'website_event'],
+
+    # always loaded
+    'data': [
+        'views/event_view.xml',
+        'data/email_template_data.xml',
+        # ~ 'views/res_config_settings_view.xml',
+    ],
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
